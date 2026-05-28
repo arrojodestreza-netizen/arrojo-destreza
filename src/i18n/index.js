b3d0221d902df849a44cc44d03640647def3ec6a
arrojodestreza-netizen <arrojo.destreza@gmail.com>
2026-05-28 18:59:15 +0100

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,1 +1,34 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import pt from './locales/pt.json';
+import es from './locales/es.json';
+import en from './locales/en.json';
+import fr from './locales/fr.json';
+import de from './locales/de.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      pt: { translation: pt },
+      es: { translation: es },
+      en: { translation: en },
+      fr: { translation: fr },
+      de: { translation: de },
+    },
+    fallbackLng: 'pt',
+    supportedLngs: ['pt', 'es', 'en', 'fr', 'de'],
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
 
